78e1c7982fea11625085aba93fd3f8ca91d01fe0
lukasbuschdev <buschlukas175@gmail.com>
2024-10-20 19:03:40 -0600

    fixed scroll button visibility in privacy

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.scss']
 })
 export class PrivacyComponent {
+  isScrollButtonVisible = false;
+
   constructor(private router: Router, public sharedService: SharedService) {}
 
+  ngOnInit() {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrollButtonVisible = scrollPosition > 300;
+  }
+
   closePrivacy() {
-    this.router.navigate(['/main-content'])
+    this.router.navigate(['/main-content']);
   }
 
   scrollUp() {
